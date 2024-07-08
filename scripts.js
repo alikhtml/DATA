0387129b216ee8f8fc0b5e409ecbd8f800d7c0c6
alikhtml <139446726+alikhtml@users.noreply.github.com>
2024-07-09 01:02:08 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 function register() {
     document.getElementById('registerForm').style.display = 'none';
     document.getElementById('buttonContainer').style.display = 'flex';
+    requestNotificationPermission();
 }
 
 function openLink(url) {
     window.open(url, '_blank');
+}
+
+function requestNotificationPermission() {
+    if ("Notification" in window) {
+        Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                new Notification("Спасибо за регистрацию!");
+            }
+        });
+    }
 }
