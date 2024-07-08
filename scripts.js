09ab7d51b2615036664600e988b60543c00db162
alikhtml <139446726+alikhtml@users.noreply.github.com>
2024-07-09 00:47:39 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function register() {
+    document.getElementById('registerForm').style.display = 'none';
+    document.getElementById('buttonContainer').style.display = 'flex';
+}
+
+function openLink(url) {
+    window.open(url, '_blank');
+}
